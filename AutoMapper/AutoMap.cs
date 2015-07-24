b10b6a3dcdd333cc9be3fcccb8409504ddb9cb6e
Venu Bukka <venugupta@users.noreply.github.com>
2015-07-24 15:22:02 +0530

    Initial code to auto-map

---
+++ AutoMapper/AutoMap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace AutoMapper
+{
+    public static class AutoMap
+    {
+        public interface IMappable
+        { }
+
+        public static IMappable Map(this IMappable sourceMap, IMappable destMap)
+        {
+            destMap.GetType().GetFields().ToList().ForEach(
+                            f =>
+                            {
+                                f.SetValue(destMap, sourceMap.GetType().GetField(f.Name)?.GetValue(sourceMap));
+                            }
+                );
+
+            destMap.GetType().GetProperties().ToList().ForEach(
+                            p =>
+                            {
+                                p.SetValue(destMap, sourceMap.GetType().GetProperty(p.Name)?.GetValue(sourceMap));
+                            }
+                );
+
+            return destMap;
+        }
+
+        public static void ContinueWith(this IMappable source, Action<IMappable> action)
+        {
+            action?.Invoke(source);
+        }
+    }
+}
